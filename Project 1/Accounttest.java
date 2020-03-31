b253578629651ffd7ee91f1a34d8bacb656cdd4e
hossn75 <62230593+hossn75@users.noreply.github.com>
2020-03-30 20:56:32 -0400

    Create Accounttest.java

---
+++ Project 1/Accounttest.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class AccountTest{
+	public static void main (String [] args) {
+		//two account OBJECTs
+	Account account1 = new Account("Jane Green ", 50.00);
+	Account account2 = new Account("John blue", -7.53);
+	System.out.printf("%s balance: %.2f%n",account1.getName(), account1.getBalance());
+	System.out.printf("%s balance: %.2f%n", account2.getName(),account2.getBalance());
+	Scanner input = new Scanner(System.in);
+	System.out.printf("Enter depositit maount for account1:");
+	double depositAmount = input.nextDouble();
+	System.out.printf("%nadding %.2f to account1 balance%n%n",depositAmount);
+	
+	System.out.printf("%s balance: $%.2f%n",account1.getName(),account1.getBalance());
+	System.out.printf("%s balance: $%.2f%n",account2.getName(),account2.getBalance());
+	
+	System.out.print("Enter deposit amount for account2");
+	depositAmount = input.nextDouble();
+	System.out.printf("%nadding %.2f to account1 balance%n%n", depositAmount);
+	account2.deposit(depositAmount);
+	
+	System.out.printf("%s balance: $%.2f%n",account1.getName(),account1.getBalance());
+	System.out.printf("%s balance: $%.2f%n",account2.getName(),account2.getBalance());
+	
+	}	
+	
+}
